3fa8af59a9cc58f87cdf25faa144390ca9272d88
RooGhz720 <rooghz720@gmail.com>
2023-02-16 09:14:40 +0800

    perf initial
    Signed-off-by: RooGhz720 <rooghz720@gmail.com>

--- kramel.sh
+++ kramel.sh
@@ -1,46 +1,46 @@
 #!/usr/bin/env bash
 echo "Cloning dependencies"
-git clone --depth=1 https://github.com/stormbreaker-project/kernel_xiaomi_sweet -b V2 kernel
+git clone --depth=1 -b main https://github.com/RooGhz720/perfx_sweet_kernel kernel
 cd kernel
 git clone --depth=1 -b master https://github.com/MASTERGUY/proton-clang clang
-git clone --depth=1 https://github.com/stormbreaker-project/AnyKernel3 -b sweet AnyKernel
+git clone --depth=1 -b incog https://github.com/RooGhz720/Anykernel3 AnyKernel
 echo "Done"
 TANGGAL=$(date +"%F-%S")
 START=$(date +"%s")
-export CONFIG_PATH=$PWD/arch/arm64/configs/sweet_user_defconfig
+export CONFIG_PATH=$PWD/arch/arm64/configs/sweet_defconfig
 PATH="${PWD}/clang/bin:$PATH"
 export ARCH=arm64
-export KBUILD_BUILD_HOST="Drone-CI"
-export KBUILD_BUILD_USER="mtpiplod"
+export KBUILD_BUILD_HOST="MyLabs"
+export KBUILD_BUILD_USER="RooGhz720"
 # Send info plox channel
 function sendinfo() {
-    curl -s -X POST "https://api.telegram.org/bot$BOTTOKEN/sendMessage" \
-        -d chat_id="$CHATID" \
+    curl -s -X POST "https://api.telegram.org/bot$BOT_API/sendMessage" \
+        -d chat_id="$CHAT_ID" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=html" \
-        -d text="<b>• Sweet-Stormbreaker Kernel •</b>%0ABuild started on <code>Circle CI</code>%0AFor device <b>Redmi Note 10 Pro/Max</b> (sweet/sweetin)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>Proton clang 13</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b>"
+        -d text="perf kernel sweet builbot drone herness"
 }
 # Push kernel to channel
 function push() {
     cd AnyKernel
     ZIP=$(echo *.zip)
-    curl -F document=@$ZIP "https://api.telegram.org/bot$BOTTOKEN/sendDocument" \
-        -F chat_id="$CHATID" \
+    curl -F document=@$ZIP "https://api.telegram.org/bot$BOT_API/sendDocument" \
+        -F chat_id="$CHAT_ID" \
         -F "disable_web_page_preview=true" \
         -F "parse_mode=html" \
         -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Redmi Note 10 Pro/Max (sweet)</b>"
 }
 # Fin Error
 function finerr() {
-    curl -F document=@$LOG "https://api.telegram.org/bot$BOTTOKEN/sendDocument" \
-        -F chat_id="$CHATID" \
+    curl -F document=@$LOG "https://api.telegram.org/bot$BOT_API/sendDocument" \
+        -F chat_id="$CHAT_ID" \
         -F "disable_web_page_preview=true" \
         -F "parse_mode=html" \
         -F caption="Build logs"
 }
 # Compile plox
 function compile() {
- make sweet_user_defconfig O=out
+ make sweet_defconfig O=out
     make -j$(nproc --all) O=out \
                       ARCH=arm64 \
                       CC=clang \
@@ -55,7 +55,7 @@ function compile() {
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 Sweet-StormBreaker-V2-${TANGGAL}.zip *
+    zip -r9 perf-kernel-sweet-${TANGGAL}.zip *
     cd ..
 }
 sendinfo
